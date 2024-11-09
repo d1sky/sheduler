c86a3b3a8b4a84304b52e61830d4412fa94c53ba
d1sky <godasin@gmail.com>
2024-11-09 05:13:00 +0300

    week header number size and short day name + styles

--- src/components/week-header/week-header.jsx
+++ src/components/week-header/week-header.jsx
@@ -1,18 +1,26 @@
 import { useSelector } from 'react-redux';
 import { getActiveDate } from '../../services/active-date-slice';
-import { getFirstDateOfCurrentWeek, getFullDayNameOfWeek } from '../../utils/date';
-import './week-header.css';
+import { getFirstDateOfCurrentWeek, getShortDayNameOfWeek } from '../../utils/date';
+import styles from './week-header.module.css';
+
+Date.prototype.getWeek = function () {
+    let onejan = new Date(this.getFullYear(), 0, 1);
+    let weekNumber = Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+
+    return weekNumber === 53 ? 1 : weekNumber
+}
+
 
 
 export const WeekHeader = () => {
     const activeDate = useSelector(getActiveDate)
 
+
+
     return (
-        <div className="week_header_container">
-            <div className="week_header_hour">
-                <div className="week_header_hour_content">
-                    {` `}
-                </div>
+        <div className={styles.container}>
+            <div className={styles.week_number}>
+                {`${(new Date(getFirstDateOfCurrentWeek(activeDate))).getWeek()} нед.`}
             </div>
 
             {[...Array(7).keys()].map(i => {
@@ -20,8 +28,13 @@ export const WeekHeader = () => {
                 currentDate.setDate(currentDate.getDate() + i)
 
                 return (
-                    <div key={i} className="week_header_day">
-                        {`${getFullDayNameOfWeek(currentDate)} ${currentDate.getDate()}`}
+                    <div key={i} className={styles.week_day}>
+                        <div className={styles.day_number}>
+                            {currentDate.getDate()}
+                        </div>
+                        <div className={styles.day_name}>
+                            {`${getShortDayNameOfWeek(currentDate)} `}
+                        </div>
                     </div>
                 )
             }
